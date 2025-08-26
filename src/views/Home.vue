5b213691a904f07d0438c78c4aac98e1542d3e70
luke <kamau.luke.m@gmail.com>
2025-08-26 06:44:59 +0300

    Add Toast animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-    <Toast v-if="showToast" />
+    <transition name="toast">
+      <Toast v-if="showToast" />
+    </transition>
     <Todos @bad-value="triggerToast" />
   </div>
 </template>
@@ -19,3 +21,29 @@ const triggerToast = () => {
   }, 3000);
 };
 </script>
+
+<style scoped>
+.toast-enter-from {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+.toast-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+.toast-enter-active {
+  transition: all 1s ease-in;
+}
+
+.toast-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+.toast-leave-to {
+  opacity: 0;
+  transform: translateY(60px);
+}
+.toast-leave-active {
+  transition: all 1s ease-out;
+}
+</style>
